f23507298c73d9521bba20f2ce0d216e30fe6b62
Denis Jackman <denis_jackman@hotmail.com>
2022-06-23 07:47:04 +0100

    coding game update

--- codinggame.py
+++ codinggame.py
@@ -1,19 +1,64 @@
+'''
+    code wars test bed
+'''
 import sys
 import math
 
-# Auto-generated code below aims at helping you parse
-# the standard input according to the problem statement.
 
-l = int(input())
-h = int(input())
-t = input()
-list="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
-# for letter in t:
+def change_to_binary(a):
+    '''
+        add binary function
+    '''
+    # your code here
+    result = ''
+    c = ''
+    for item in a:
+        n = ord(item)
+        c = "{0:b}".format(n).zfill(7)
+        result += c
+        c = ''
+    return result
 
-# for i in range(h):
-#     row = input()
 
-# Write an answer using print
-# To debug: print("Debug messages...", file=sys.stderr, flush=True)
+def change_to_code(text):
+    result = ''
+    code_one = "0 "
+    count_one = 0
+    store_one = ''
+    code_zero = "00 "
+    count_zero = 0
+    store_zero = ''
 
-print("answer")
+    for item in text:
+        if item == '1':
+            count_one += 1
+            store_one += '0'
+            if count_zero > 0:
+                result += code_zero + store_zero + ' '
+                store_zero = ''
+                count_zero = 0
+        if item == '0':
+            count_zero += 1
+            store_zero += '0'
+            if count_one > 0:
+                result += code_one + store_one + ' '
+                store_one = ''
+                count_one = 0
+    if count_zero > 0:
+        result += code_zero + store_zero
+    if count_one > 0:
+        result += code_one + store_one
+
+    return result
+
+
+def main():
+    '''
+        main function
+    '''
+    print(change_to_binary('%'))
+    print(change_to_code(change_to_binary('%')))
+
+
+if __name__ == '__main__':
+    main()
--- new_scratch.py
+++ new_scratch.py
@@ -1,12 +1,63 @@
 '''
     code wars test bed
 '''
+import sys
+import math
+
+
+def change_to_binary(a):
+    '''
+        add binary function
+    '''
+    # your code here
+    result = ''
+    c = ''
+    for item in a:
+        n = ord(item)
+        c = "{0:b}".format(n).zfill(7)
+        result += c
+        c = ''
+    return result
+
+
+def change_to_code(text):
+    result = ''
+    code_one = "0 "
+    count_one = 0
+    store_one = ''
+    code_zero = "00 "
+    count_zero = 0
+    store_zero = ''
+
+    for item in text:
+        if item == '1':
+            count_one += 1
+            store_one += '0'
+            if count_zero > 0:
+                result += code_zero + store_zero + ' '
+                store_zero = ''
+                count_zero = 0
+        if item == '0':
+            count_zero += 1
+            store_zero += '0'
+            if count_one > 0:
+                result += code_one + store_one + ' '
+                store_one = ''
+                count_one = 0
+    if count_zero > 0:
+        result += code_zero + store_zero
+    if count_one > 0:
+        result += code_one + store_one
+
+    return result
 
 
 def main():
     '''
         main function
     '''
+    print(change_to_binary('%'))
+    print(change_to_code(change_to_binary('%')))
 
 
 if __name__ == '__main__':
